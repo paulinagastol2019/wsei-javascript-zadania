2013deca10685598837ccc51e75ebc5d9e4a80bb
Paulina <Paulina@DESKTOP-98M0J3R>
2019-10-07 17:57:01 +0200

    zad. 1 - funkcje czasu

---
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -0,0 +1,13 @@
+function countHello(num) {
+    var counter = 1;
+    var intervalID = setInterval(()=>{
+        console.log('Hello' + counter++);
+
+        if (counter == num) {
+            clearInterval(intervalId);
+        }
+        counter++;
+    }, 500);
+
+}
+countHello(4);
