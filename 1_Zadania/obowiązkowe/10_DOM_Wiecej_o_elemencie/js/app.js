057a47e619278f07a32d8a2908d995cc152177cd
Gąstoł Paulina <paulina.gastol@microsoft.wsei.edu.pl>
2019-12-22 14:11:15 +0100

    zmiana

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -3,4 +3,49 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
-});
+
+// zadanie 0
+
+const elements = document.querySelectorAll('.ex5 li');
+for (let i=0; i<elements.length; i++){
+
+    if( i % 2 == 0) {
+        elements[i].getElementsByClassName.backgroundcolor = 'green'
+    }
+
+    if (i% 3 == 0) {
+        elements[i].style.textDecoration = 'underline';
+    }
+     
+    elements[4].className = 'big';
+
+}
+
+// zadanie 1
+
+let chromeElement = document.querySelector('.chrome');
+let edgeElement = document.querySelector('.edge');
+let firefoxElement = document.querySelector('.firefox');
+
+edgeElement.style.backgroundImage ='url(../assets/img/edge.png';
+firefoxElement.style.backgroundImage ='.url(../assets/img/firefox.png)';
+
+chromeElement.style.width = '100px'; 
+
+//zadanie 2
+
+let name = document.querySelector('.name');
+let color = document.querySelector('#fav_color');
+let meal = document.querySelector('#fav_meal');
+
+nameSpam.innerHTML = 'Kamil';
+color.innerHTML = 'niebieski';
+meal.innerHTML = 'oscypek';
+
+let ex3=document.querySelector('.ex3');
+ex3.querySelector('ul').classList.add('menu');
+
+let ex3=document.querySelector
+
+
+}}
---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
@@ -0,0 +1,23 @@
+//zadanie 0a
+
+let parents =document.querySelectorAll('.parent');
+parents.forEach( e => e.addEventListener)
+
+for (let i = 0; i < parents.length; ++i ){
+
+addEvents(parents[1]);
+
+}
+
+
+
+function addEvents(element) {
+
+    element.addEventListener('mouseover', function() {
+
+        this.children[0].style.display = 'block';
+    })
+    element.addEventListener('mouseleave', function(){
+        this.children[0].style.display = 'none';
+    })
+}
